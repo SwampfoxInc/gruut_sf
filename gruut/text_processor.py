82a0a501663a93b89ee79b24578f73736a3707e8
Roshan Thomas <rthomas@swampfoxinc.com>
2024-09-04 15:19:45 -0400

    remove wrong param

--- gruut/text_processor.py
+++ gruut/text_processor.py
@@ -212,7 +212,6 @@ class TextProcessor:
                                 is_major_break=is_major_break,
                                 lang=get_lang(node.lang),
                                 voice=node.voice,
-                                rate=node.voice,
                                 pause_before_ms=word_pause_before_ms,
                                 marks_before=(
                                     word_marks_before if word_marks_before else None
