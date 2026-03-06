a784bed445dd78831c120268ee8e6f4f255a0dfa
Sergey Razin <techdozor@users.noreply.github.com>
2026-03-06 15:17:36 -0500

    Update apostrophe replacement for normalization

--- gruut/lang.py
+++ gruut/lang.py
@@ -835,7 +835,7 @@ def get_fr_settings(lang_dir=None, **settings_args) -> TextProcessorSettings:
         "end_punctuations": {'"', "”", "»", "]", ")", ">"},
         "default_currency": "EUR",
         "default_date_format": InterpretAsFormat.DATE_DMY_ORDINAL,
-        "replacements": [("'", "'")],  # normalize apostrophe
+        "replacements": [("’", "'")],  # normalize apostrophe
         "post_process_sentence": fr_post_process_sentence,
         "address_abbreviations": FR_ADDRESS_ABBREVIATIONS,
         **settings_args,
